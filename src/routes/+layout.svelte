ef7d5ed5b1701ea7f074c50808b64b591dd0b88e
Sahin Vardar <sahin@vardar.io>
2023-05-05 16:52:57 +0200

    clean: cleaned doc site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,7 @@
 
   $: activeUrl = $page.url.pathname;
   let logo = '/images/flowbite-svelte-icon-logo.svg';
-  let divClass = 'w-full ml-auto lg:block lg:w-auto order-1 lg:order-none';
+  let divClass = 'w-full mx-auto lg:block lg:w-auto order-1 lg:order-none';
   let ulClass =
     'flex flex-col py-3 my-4 lg:flex-row lg:my-0 text-sm font-medium text-gray-900 dark:text-gray-300 gap-4';
 
@@ -48,12 +48,6 @@
     <span hidden={$page.route.id === '/'}>
       <NavSidebarHamburger on:click={toggleDrawer} btnClass="mr-3 m-0 mr-3 lg:hidden" />
     </span>
-    <NavBrand href="/">
-      <img src={logo} class="mr-3 h-8" alt="Flowbite Svelte Logo" />
-      <span class="self-center whitespace-nowrap text-2xl font-semibold text-gray-900 dark:text-white">
-        Flowbite Svelte
-      </span>
-    </NavBrand>
 
     {#if !isHomePage}
       <AlgoliaSearch />
@@ -79,40 +73,7 @@
         class="lg:px-2 lg:mb-0"
         active={activeUrl.startsWith('/docs/components/accordion')}
         href="/docs/components/accordion">Components</NavLi>
-      <NavLi
-        class="lg:px-2 lg:mb-0"
-        active={activeUrl.startsWith('/figma')}
-        href="https://flowbite.com/figma/">Figma</NavLi>
-      <NavLi class="lg:px-2 lg:mb-0" href="https://flowbite-svelte-blocks.vercel.app/">Blocks</NavLi>
     </NavUl>
-
-    <div class="flex items-center ml-auto">
-      <ToolbarLink
-        class="hidden sm:inline-block dark:hover:text-white hover:text-gray-900"
-        name="View on GitHub"
-        href="https://github.com/themesberg/flowbite-svelte">
-        <GitHub /></ToolbarLink>
-      <ToolbarLink
-        class="hidden xl:inline-block dark:hover:text-white hover:text-gray-900"
-        name="Join community on Discord"
-        href="https://discord.gg/4eeurUVvTy"><Discord /></ToolbarLink>
-      <ToolbarLink
-        class="hidden xl:inline-block dark:hover:text-white hover:text-gray-900"
-        name="Subscribe to YouTube channel"
-        href="https://www.youtube.com/channel/UC_Ms4V2kYDsh7F_CSsHyQ6A">
-        <YouTube />
-      </ToolbarLink>
-      <DarkMode size="lg" class="inline-block dark:hover:text-white hover:text-gray-900" />
-      <Tooltip class="dark:bg-gray-900" placement="bottom-end">Toggle dark mode</Tooltip>
-    </div>
-    <a href="https://www.npmjs.com/package/flowbite-svelte" class="hidden sm:block">
-      <DocBadge
-        large
-        class="ml-2 xl:ml-6 hover:bg-primary-600 hover:text-white dark:hover:bg-primary-800 dark:hover:text-white">
-        v{version} 
-      </DocBadge>
-    </a>
-
     <NavHamburger on:click={toggle} btnClass="ml-3 m-0 lg:hidden {isHomePage ? '' : 'hidden'}" />
   </Navbar>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
   import { MetaTags } from 'svelte-meta-tags';
   import type { PageData } from './$types';
-  import Components from './landing/Components.svelte';
-  import Featured from './landing/Featured.svelte';
-  import Hero from './landing/Hero.svelte';
-  import Footer from './utils/Footer.svelte';
-  import Contributors from './landing/Contributors.svelte';
-  import DesignFigma from './landing/DesignFigma.svelte';
-  import GetStarted from './landing/GetStarted.svelte';
-  import SocialProof from './landing/SocialProof.svelte';
-  import CTA from './landing/CTA.svelte';
+  import Components from './landing/Components.svelte';  
+  import GetStarted from './landing/GetStarted.svelte';  
 
   export let data: PageData;
 
-  let title = 'Flowbite Svelte - UI Component Library';
+  let title = 'svelte-ui-kit';
   let description =
-    'Flowbite Svelte is an open-source UI component library built with Svelte components, Tailwind CSS utility classes and based on the Flowbite design system and components.';
+    'svelte-ui-kit based on Flowbite Svelte';
   let default_title = 'Svelte UI Components';
   const image = `https://open-graph-vercel.vercel.app/api/flowbite-svelte?title=${default_title}`;
 </script>
@@ -42,13 +35,6 @@
   }} />
 
 <main class="flex-auto min-w-0 lg:static lg:max-h-full lg:overflow-visible divide-y dark:divide-gray-700">
-  <Hero />
-  <Featured />
   <Components {data} />
-  <CTA />
-  <SocialProof />
-  <DesignFigma />
-  <Contributors {data} />
   <GetStarted />
-  <Footer />
 </main>
